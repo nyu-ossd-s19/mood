bd73008686926c2ec61ad397f11d5b2c173633f6
Candace Johnson <ccj286@nyu.edu>
2019-02-10 14:06:10 -0500

    added a script

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function main() {
+    const today = new Date();
+    // todo use OpenWeatherMaps API for the current
+    // const weather = ''; 
+    // todo use Giphy API to get the gif we want to use for the session
+
+    console.log("hello from line 8 of index.js");
+    
+}
+
+document.addEventListener('DOMContentLoaded', main, false);
